c7961b2579af8fb9aec5d458a2f771ce5b1f0b22
Audrey Roy <audreyr@gmail.com>
2013-07-09 01:42:18 +0200

    Accept command-line arg for number of minutes.

--- talk_timer.py
+++ talk_timer.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+import argparse
 from time import sleep
 import pyttsx
 
-# Sleep for 5 min lightning talk duration
-# TODO: make this a command line arg
-sleep(60 * 5)
+parser = argparse.ArgumentParser(description='Presentation timer with spoken warnings.')
+parser.add_argument('minutes', type=int, nargs=1, help='Length of talk in minutes')
+args = parser.parse_args()
+
+minutes = args.minutes[0]
+
+print "Timing a {0}-minute talk.".format(minutes)
+
+# Sleep for X-minute talk duration
+sleep(minutes * 60)
 
 # Text to speech
 engine = pyttsx.init()
