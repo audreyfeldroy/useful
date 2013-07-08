b05f95fb3e67ef2d13d67bf369e10c679a8b55cf
Audrey Roy <audreyr@gmail.com>
2013-07-08 14:11:44 +0200

    Add script to find site-packages dirs.

---
+++ find_site_packages.py
@@ -0,0 +1,11 @@
+import site
+from distutils.sysconfig import get_python_lib
+
+print("Your main site-packages directory:")
+print(get_python_lib())
+
+print
+
+print("All global site-packages directories on your system:")
+print(site.getsitepackages())
+
