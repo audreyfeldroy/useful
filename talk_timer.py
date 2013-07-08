07538222d07b0a565cadadd40df9bedeb12a4f60
Audrey Roy <audreyr@gmail.com>
2013-07-09 01:43:25 +0200

    Remove nargs param so that minutes is an int, not list.

--- talk_timer.py
+++ talk_timer.py
@@ -5,15 +5,13 @@ from time import sleep
 import pyttsx
 
 parser = argparse.ArgumentParser(description='Presentation timer with spoken warnings.')
-parser.add_argument('minutes', type=int, nargs=1, help='Length of talk in minutes')
+parser.add_argument('minutes', type=int, help='Length of talk in minutes')
 args = parser.parse_args()
 
-minutes = args.minutes[0]
-
-print "Timing a {0}-minute talk.".format(minutes)
+print "Timing a {0}-minute talk.".format(args.minutes)
 
 # Sleep for X-minute talk duration
-sleep(minutes * 60)
+sleep(args.minutes * 60)
 
 # Text to speech
 engine = pyttsx.init()
